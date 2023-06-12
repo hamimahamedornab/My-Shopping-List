4c9c33679d43740808407252b8bce432c5f5757e
Hamim Ahamed Ornab <75578573+hamimahamedornab@users.noreply.github.com>
2023-06-12 23:35:20 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,27 +39,44 @@ class ShoppingListPage extends StatelessWidget {
         ],
       ),
       body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
+        padding: EdgeInsets.all(20.0),
+
+        children: const [
           ListTile(
             leading: Icon(Icons.apple),
+            textColor: Colors.black,
+            titleTextStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),
+            iconColor: Colors.green,
             title: Text('Apple'),
           ),
           ListTile(
             leading: Icon(Icons.local_pizza),
+            textColor: Colors.black,
+            titleTextStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),
+            iconColor: Colors.yellow,
             title: Text('Pizza'),
           ),
           ListTile(
             leading: Icon(Icons.rice_bowl),
+            textColor: Colors.black,
+            titleTextStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),
+            iconColor: Colors.blue,
             title: Text('Rice Bowl'),
           ),
           ListTile(
             leading: Icon(Icons.soup_kitchen),
+            textColor: Colors.black,
+            titleTextStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),
+            iconColor: Colors.redAccent,
             title: Text('Soup'),
           ),
           ListTile(
             leading: Icon(Icons.mobile_friendly),
-            title: Text('Mobile'),
+            iconColor: Colors.pink,
+            textColor: Colors.black,
+            titleTextStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),
+            title: Text('Mobile',),
+
           ),
         ],
       ),
